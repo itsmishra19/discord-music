d75da93e2b90689c656c5da48e821049ee928999
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-04-06 21:21:06 +0530

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -4,14 +4,13 @@ const emotes = require ("../config/emojis.json")
 
 module.exports.run = async (client, message, args) => {
 
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`)
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
+    
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
   
-    let song = await client.player.pause(message.guild.id);
-
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`)
+  let song = await client.player.pause(message.guild.id);
             
-    message.channel.send(`**Song ${song.name} paused ${emotes.success}**`);
-
+  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.pause} | Paused!` }})
     
 }
 

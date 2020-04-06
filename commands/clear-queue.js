36d2e86c136972d1101da75d26555e0ff4fbd44c
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-04-06 20:28:03 +0530

    Added embed

--- commands/clear-queue.js
+++ commands/clear-queue.js
@@ -1,17 +1,15 @@
 const Discord = require("discord.js")
 const fs = require("fs")
-const emotes = require ("../config/emojis.json")
 
 module.exports.run = async (client, message, args) => {
-  
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`)
+ 
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: 0x00FF70, description: `${client.emotes.error} | You must be in a voice channel!` }})
 
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`)
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: 0x00FF70, description: `${client.emotes.error} | There is nothing playing!` }})
 
-    client.player.clearQueue(message.guild.id);
+  client.player.clearQueue(message.guild.id);
 
-    message.channel.send(`**Queue cleared ${emotes.success}**`);
-
+  return message.channel.send({embed: {color: 0x00FF70, description: `${client.emotes.success} | Queue cleared!` }})
 
 }
 

b17a001c623b834584c2439b508dbd5891df613c
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-04-06 21:30:00 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,30 +1,29 @@
 const Discord = require("discord.js")
 const fs = require("fs")
-const emotes = require ("../config/emojis.json")
 
 module.exports.run = async (client, message, args) => {
+let queue = args.join(" ");
+if (!queue) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Please enter a query to search!` }})
 
-    if (!args[0]) return message.channel.send(`**Please enter a music ${emotes.error}**`)
+let playing = client.player.isPlaying(message.guild.id);
 
-let aSongIsAlreadyPlaying = client.player.isPlaying(message.guild.id);
-
-if(aSongIsAlreadyPlaying){
+if(playing){
     // Add the song to the queue
-    let song = await client.player.addToQueue(message.guild.id, args.join(" "));
-    message.channel.send(`**${song.name} added to queue **`);
+    let song = await client.player.addToQueue(message.guild.id, queue);
+    message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.success} | ${song.name} Added to the queue!` }})
 } else {
     // Else, play the song
-    let song = await client.player.play(message.member.voice.channel, args.join(" "));
-    message.channel.send(`**Currently playing ${song.name} ${emotes.music}**`);
+    let song = await client.player.play(message.member.voice.channel, queue);
+    message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.music} | Now Playing:\n${song.name}` }})
     song.queue.on('end', () => {
-        message.channel.send('**The queue is empty, please add some songs!**');
+    message.channel.send({embed: {color: client.colors.warning, description: `${client.emotes.warning} | Queue completed, add some more songs to play!` }})
     });
 
     song.queue.on('songChanged', (oldSong, newSong, skipped, repeatMode) => {
         if(repeatMode){
-            message.channel.send(`**Repeating, ${newSong}**`);
+            message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.repeat} | Repeating:\n ${oldSong.name}` }})
         } else {
-            message.channel.send(`Now playing ${newSong}...`);
+            message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.music} | Now Playing:\n ${newSong.name}` }})
         }
     });
 }

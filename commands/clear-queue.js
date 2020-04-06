26e8e4024ae71e8accb4b9a013c553ad6b50aa61
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-04-06 20:34:48 +0530

    Update clear-queue.js

--- commands/clear-queue.js
+++ commands/clear-queue.js
@@ -3,13 +3,13 @@ const fs = require("fs")
 
 module.exports.run = async (client, message, args) => {
  
-  if(!message.member.voice.channel) return message.channel.send({embed: {color: 0x00FF70, description: `${client.emotes.error} | You must be in a voice channel!` }})
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
 
-  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: 0x00FF70, description: `${client.emotes.error} | There is nothing playing!` }})
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
 
   client.player.clearQueue(message.guild.id);
 
-  return message.channel.send({embed: {color: 0x00FF70, description: `${client.emotes.success} | Queue cleared!` }})
+  return message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.success} | Queue cleared!` }})
 
 }
 

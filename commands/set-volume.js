314b226ad52cf9607df04a8aae5f9caed27b36dd
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-04-07 07:53:35 +0530

    Update set-volume.js

--- commands/set-volume.js
+++ commands/set-volume.js
@@ -4,15 +4,16 @@ const emotes = require ("../config/emojis.json")
 
 module.exports.run = async (client, message, args) => {
 
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`)
-
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`)
-    if (!args[0]) return message.channel.send(`**Please enter a number ${emotes.error}**`)
-    if (isNaN(args[0])) return message.channel.send(`**Please enter a valid number ${emotes.error}**`)
-  
-    client.player.setVolume(message.guild.id, parseInt(args.join(" ")));
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
     
-    message.channel.send("**Volume set to** `" +  args.join(" ") + "`" + `** ** ${emotes.success}`);
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
+  let volume = parseInt(args.join(" "));
+  if (!volume) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Please enter a number!` }})
+  if (isNaN(args[0])) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Please enter a valid number!` }})
+  
+  client.player.setVolume(message.guild.id, volume);
+    
+  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.success} | Volume set to \`${args.join(" ")}\` ` }})
 
 
 }

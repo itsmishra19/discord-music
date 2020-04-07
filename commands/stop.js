521ad615f840f8294d077967780998196eaa394b
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-04-07 08:01:22 +0530

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,21 +1,20 @@
 const Discord = require("discord.js")
 const fs = require("fs")
-const emotes = require ("../config/emojis.json")
 
 module.exports.run = async (client, message, args) => {
-
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`)
-
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`)
-
-    client.player.stop(message.guild.id);
+  
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
     
-    message.channel.send(`**Music stopped ${emotes.success}**`);
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
+  
+  let song = await client.player.stop(message.guild.id);
+
+  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.stop} | Disconnected!` }})
 
 
 }
 
 module.exports.config = {
   name: "stop",
-  aliases: ['dc','disconnect']
+  aliases: ['dc',"disconnect"]
 }

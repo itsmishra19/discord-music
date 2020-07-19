be270e4bf57a1360a173560fae47952f5b79f509
Kylian <admin@skyfra.tech>
2020-07-19 12:26:14 +0200

    fix queue
    change the song to tracks since it doesn't work.

--- commands/queue.js
+++ commands/queue.js
@@ -9,8 +9,8 @@ module.exports.run = async (client, message, args) => {
 
     if(!queue) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
 
-    let q = queue.songs.map((song, i) => {
-        return `${i === 0 ? 'Current' : `${i+1}`}- ${song.name} : ${song.author}`
+    let q = queue.tracks.map((tracks, i) => {
+        return `${i === 0 ? 'Current' : `${i+1}`}- ${tracks.name} : ${tracks.author}`
     }).join('\n');  
        message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.queue} | ${q}` }})
 

3e158ebbf9e79a16157a38c39583fac4f627965e
HYDROXIN <40756526+inhydrox@users.noreply.github.com>
2020-08-07 08:02:59 +0530

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -5,11 +5,13 @@ module.exports.run = async (client, message, args) => {
 
   if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
     
+  if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You are not in my voice channel!`}});
+  
   if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
   
   let song = await client.player.pause(message.guild.id);
             
-  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.pause} | Paused!` }})
+  return message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.pause} | Paused!` }});
     
 }
 

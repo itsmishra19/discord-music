a646a7f53fdf58f6e667237f2b27ec7d5f759fd9
HYDROXIN <40756526+inhydrox@users.noreply.github.com>
2020-08-07 08:02:25 +0530

    Update now-playing.js

--- commands/now-playing.js
+++ commands/now-playing.js
@@ -3,8 +3,6 @@ const fs = require("fs")
 
 module.exports.run = async (client, message, args) => {
 
-    if(!message.member.voice.channel) return  message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
-
     if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
 
     let song = await client.player.nowPlaying(message.guild.id);

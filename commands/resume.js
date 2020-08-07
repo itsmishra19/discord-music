0cf795a8db503b42ae3343b9c05af56f4883b379
HYDROXIN <40756526+inhydrox@users.noreply.github.com>
2020-08-07 08:03:27 +0530

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -5,11 +5,14 @@ module.exports.run = async (client, message, args) => {
 
   if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
     
+  if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You are not in my voice channel!`}});
+
+  
   if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
   
   let song = await client.player.resume(message.guild.id);
             
-  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.resume} | Resumed!` }})
+  return message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.resume} | Resumed!` }});
     
 }
 

bc4ca26df15e692281993645e7501617e5541e6b
HYDROXIN <40756526+inhydrox@users.noreply.github.com>
2020-08-07 08:00:08 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "prefix": "!",
+    "token_bot": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "colors": {
+      "error": "0xFF0000",
+      "success": "0x00FF46",
+      "warning": "0xFFD100"
+    },
+    "emotes": {
+    "music": "🎶 ",
+    "queue": "🎞️",
+    "error": "❌",
+    "success": "☑️",
+    "warning": "⚠️",
+    "repeat": "🔁",
+    "pause": "⏸️",
+    "resume": "⏯️",
+    "stop": "⏹️"
+    }
+}

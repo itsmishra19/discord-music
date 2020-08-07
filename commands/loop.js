fb940b3a56465ca6bce15ff622fc47dd3dc9ac41
HYDROXIN <40756526+inhydrox@users.noreply.github.com>
2020-08-07 08:01:38 +0530

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -4,6 +4,9 @@ const fs = require("fs")
 module.exports.run = async (client, message, args) => {
   
 if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
+
+if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You are not in my voice channel!`}});
+  
 if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
 
 client.player.setRepeatMode(message.guild.id, true);

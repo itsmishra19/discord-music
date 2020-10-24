186801fa0f29f343f52c87905a6dff1b16965683
HYDROX <40756526+inhydrox@users.noreply.github.com>
2020-10-24 18:45:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
     "prefix": "!",
     "token_bot": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "colors": {
+    "inviteURI": "invite url for your bot",
+    "supportServer": "supportServer" //if any
+     "colors": {
       "error": "0xFF0000",
       "success": "0x00FF46",
       "warning": "0xFFD100"

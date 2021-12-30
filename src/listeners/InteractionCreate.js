47181ef34f61d96c23d3f355f7a312786ef9a1bc
hydrox19 <hydrox19@yandex.com>
2021-12-30 19:02:30 +0530

    feat(ConnectionChecking): implement connection checking

--- src/listeners/InteractionCreate.js
+++ src/listeners/InteractionCreate.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { BaseEvent } from "../base/BaseEvent.js";
 import { CommandContext } from "../structures/CommandContext.js";
+import { makeEmbed } from "../utils/makeEmbed.js";
 
 export class InteractionCreate extends BaseEvent {
     constructor(client) {
@@ -13,7 +14,25 @@ export class InteractionCreate extends BaseEvent {
             const cmd = this.client.commands.find(c => c.meta.slash.name === interaction.commandName);
             interaction.guild.restTimestamp = Date.now();
             if (cmd) {
+                const connectionChecking = cmd?.connectionChecking;
                 const context = new CommandContext(interaction);
+
+                if (connectionChecking) {
+                    if (connectionChecking.memberInVoice && !interaction.member.voice.channelId) {
+                        return interaction.reply({
+                            embeds: [
+                                makeEmbed("error", "You must be in a voice channel to use this command!", true)
+                            ]
+                        });
+                    }
+                    if (connectionChecking.dispatcherExists && !this.client.queue.has(interaction.guild.id)) {
+                        return interaction.reply({
+                            embeds: [
+                                makeEmbed("error", "No dispatcher exists for this server")
+                            ]
+                        });
+                    }
+                }
                 await interaction.deferReply();
                 await cmd.execute(context);
             }

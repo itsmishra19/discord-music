a4c6f748ddeb9e37334e3393d91227c333d0598f
hydrox19 <hydrox19@yandex.com>
2021-12-30 11:04:36 +0530

    feat: env_example

--- src/config.js
+++ src/config.js
@@ -2,5 +2,15 @@
 export const config = {
     token: process.env.TOKEN,
     isDev: true,
-    devGuilds: ["516073932009242624"]
+    devGuilds: ["516073932009242624"],
+    lavalink: {
+        options: {
+            moveOnDisconnect: false,
+            reconnectTries: 2,
+            restTimeout: 10000,
+            resumable: true,
+            resumableTimeout: 30
+        },
+        servers: JSON.parse(process.env.NODE ?? [])
+    }
 };

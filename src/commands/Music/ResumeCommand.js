3bf7b0888d3c7234a6b94dcd656ee005868ff93d
hydrox19 <hydrox19@yandex.com>
2021-12-30 19:50:00 +0530

    feat: ResumeCommand

---
+++ src/commands/Music/ResumeCommand.js
@@ -0,0 +1,30 @@
+import { BaseCommand } from "../../base/BaseCommand.js";
+import { makeEmbed } from "../../utils/makeEmbed.js";
+
+export class ResumeCommand extends BaseCommand {
+    constructor(client) {
+        super(client, {
+            slash: {
+                name: "resume",
+                description: "Resumes the current playing track"
+            },
+            category: "Music"
+        });
+    }
+
+    get connectionChecking() {
+        return { memberInVoice: true, dispatcherExists: true, memberInSameVoice: true };
+    }
+
+    async execute(ctx) {
+        const dispatcher = this.client.queue.get(ctx.guild.id);
+
+        await dispatcher.player?.setPaused(false);
+
+        ctx.send({
+            embeds: [
+                makeEmbed("success", `Resumes the player`)
+            ]
+        });
+    }
+}
